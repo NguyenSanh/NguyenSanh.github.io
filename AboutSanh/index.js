f8a03df728b16a8b11147a01219fec5068b70e17
NguyenSanh <jsn006@ucsd.edu>
2022-01-13 23:39:40 -0800

    Added Folder

---
+++ AboutSanh/index.js
@@ -0,0 +1,16 @@
+﻿const navToggle = document.querySelector('.nav_toggle');
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
